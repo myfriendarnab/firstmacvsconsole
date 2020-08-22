63a47b1e495806483f9ab45e6a86bab08f889ef2
myfriendarnab <myfriendarnab@gmail.com>
2020-08-23 00:13:20 +0530

    my first containerized app

---
+++ firstmacvsconsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstmacvsconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
